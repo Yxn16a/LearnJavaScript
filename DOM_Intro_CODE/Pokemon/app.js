e26361835a1498ae37e7bb9180f245bd6a836452
yves <yxn16a@acu.edu>
2022-07-02 03:08:20 +0200

    added pokemon example

--- DOM_Intro_CODE/Pokemon/app.js
+++ DOM_Intro_CODE/Pokemon/app.js
@@ -5,15 +5,23 @@ const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprite
 
 
 for (let i = 1; i <= 151; i++) {
+    // create a div 
     const pokemon = document.createElement('div');
+    // add a class to the div 
     pokemon.classList.add('pokemon');
+    // create element of type span
     const label = document.createElement('span');
+    // put the text inside the span
     label.innerText = `#${i}`;
+    //create image tag
     const newImg = document.createElement('img');
+    // create pokemon image 
     newImg.src = `${baseURL}${i}.png`
 
-
+// in the add a child  to the div the image
     pokemon.appendChild(newImg);
+    // used to add the lebel 
     pokemon.appendChild(label);
+    // add the child 
     container.appendChild(pokemon)
 }
